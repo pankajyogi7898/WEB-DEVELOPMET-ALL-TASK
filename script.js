7c8689e16a77759807a7e5143ce8ae29eba5b533
PANKAJ NATH <pyogi8461@gmail.com>
2025-11-21 19:46:51 +0530

    create random div

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ var btn = document.querySelector("button");
 
 
 btn.addEventListener("click",function(){
-    var h1 = document.createElement("h1")
-
-    var array = ["hy! my name is pankaj" , "yogi is greate" , "harsh bhaiya is god" , "sheryians coding school" , "pankaj nath yogi","keep patients"]
-
-    var arr1 = Math.floor(Math.random()*array.length)
-
-    h1.innerHTML = array[arr1]
+    var div = document.createElement("div")
+    // var h1 = document.createElement("h1")
+    // h1.innerHTML ="helllo"
     var body = document.querySelector("body")
 
     var y= Math.random()*100
     var x = Math.random()*100
-    
 
     var c1 = Math.floor(Math.random()*255)
     var c2 = Math.floor(Math.random()*255)
@@ -21,14 +16,17 @@ btn.addEventListener("click",function(){
 
     var rota = Math.floor(Math.random()*360)
 
-    // h1.style.color = `rgb(${c1},${c2},${c3})`
-    h1.style.rotate = rota + 'deg'
     
-    h1.style.position = 'absolute'
-    h1.style.left = y + '%'
-    h1.style.top = x + '%'
+    div.style.width = '100px'
+    div.style.height = '100px'
+    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    div.style.rotate = rota + 'deg'
+    
+    div.style.position = 'absolute'
+    div.style.left = y + '%'
+    div.style.top = x + '%'
 
     
-    body.appendChild(h1)
+    body.appendChild(div)
     
 })
