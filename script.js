10c095d6a432fe06d9d2c3ce67a7e6aec010dd99
PANKAJ NATH <pyogi8461@gmail.com>
2025-11-30 20:35:10 +0530

    matrix text

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-addEventListener("mousemove",function(e){
-    console.log(e.clientX,e.clientY);
-    document.body.style.setProperty("--x" , e.x + 'px');
-    document.body.style.setProperty("--y" , e.y + 'px');
+var para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const text = para.innerText;
 
+var iteration = 0
+
+para.addEventListener('mouseenter', () => {
+    setInterval(function () {
+
+        const str = text.split('').map(function (char, index) {
+            if(index < iteration){
+                return char
+            }
+            return characters.split("")[Math.floor(Math.random() * 53)]
+        }).join("")
+        para.innerText = str;
+        iteration = iteration + 0.25;
+    },30)
 })
